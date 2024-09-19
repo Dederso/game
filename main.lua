2df22da07a5668d8c6052c0a7a8d7e1fc88f447a
André Guimarães Barros <de_barros01@hotmail.com>
2024-09-19 17:24:34 -0300

    fix cam

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ function love.load()
     cameralib = require "libraries/camera"
     camera = cameralib()
     world = wf.newWorld(0, 0, true)
-    world:setGravity(0, 800)
+    world:setGravity(0, 980)
 
     love.window.setMode(800, 600, {
         resizable=true,
@@ -25,17 +25,20 @@ function love.load()
     -- Definir classes de colisão
     world:addCollisionClass('Player')
     world:addCollisionClass('Ground')
-
+    sprite_right = love.graphics.newImage("assets/Sprite_astronauta_right.png")
+    sprite_left = love.graphics.newImage("assets/Sprite_astronauta_left.png")
     player = {
-        width = 50,
-        height = 50,
-        sprite = love.graphics.newImage("assets/Sprite_astronauta.png"),
+        width = 32,
+        height = 32,
+        sprite = sprite_right,
         speed = 200,
         jumpForce = 500,
         isOnGround = false,
         direction = 1  -- 1 para direita, -1 para esquerda
     }
-    player.hitbox = world:newRectangleCollider(32, 3000, player.width, player.height)
+    local x = map.width * map.tilewidth / 2
+    local y = map.height * map.tileheight * 0.97
+    player.hitbox = world:newRectangleCollider(x, y, player.width, player.height)
     player.hitbox:setCollisionClass('Player')
     player.hitbox:setFixedRotation(true)
     player.hitbox:setFriction(0)
@@ -48,11 +51,16 @@ function love.load()
             collider:setCollisionClass('Ground')
         end
     end
+    
 end
 
 function love.update(dt)
     world:update(dt)
-    
+    if player.direction == 1 then
+        player.sprite = sprite_right
+    elseif player.direction == -1 then
+        player.sprite = sprite_left
+    end
     local vx, vy = player.hitbox:getLinearVelocity()
     local px, py = player.hitbox:getPosition()
 
@@ -84,6 +92,28 @@ function love.update(dt)
     end
 
     camera:lookAt(px, py)
+    local w = love.graphics.getWidth()
+    local h = love.graphics.getHeight()
+    local wt = map.width * map.tilewidth
+    local ht = map.height * map.tileheight
+
+
+    if camera.x < w/2 then
+        camera.x = w/2
+    end
+
+    if camera.y < h/2 then
+        camera.y = h/2
+    end
+
+    if camera.x > (wt - w/2) then
+        camera.x = (wt - w/2)
+    end
+    
+    if camera.y > (ht - h/2) then
+        camera.y = (ht - h/2)
+    end
+    
 end
 
 function love.draw()
@@ -91,13 +121,17 @@ function love.draw()
         -- Desenha o mapa
         if map then
             for i, layer in ipairs(map.layers) do
-                layer:draw()
+                if layer.name ~= "colision" then
+                    layer:draw()
+                end
             end
         else
             love.graphics.print("Mapa não carregado!", 400, 300)
         end
         -- Desenha os objetos do mundo físico (opcional)
     world:draw()
+
+
     -- Desenha o sprite do jogador centralizado
     local spriteWidth = player.sprite:getWidth()
     local spriteHeight = player.sprite:getHeight()
@@ -117,7 +151,8 @@ function love.draw()
     -- Adiciona texto de depuração
     love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), 10, 10)
     love.graphics.print("jump: " .. tostring(player.isOnGround), 10, 30)
-    
+    love.graphics.print("x: " .. tostring(player.hitbox:getX()), 10, 50)
+    love.graphics.print("y: " .. tostring(player.hitbox:getY()), 10, 70)
 end
 
 function love.keypressed(key)
