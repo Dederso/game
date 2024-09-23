38803f29288ac870f41c0a9306f6d86869b0e723
Gianluca <gianluca.mantovaneli@edu.ufes.br>
2024-09-23 09:12:50 -0300

    Melhorando o mapa para conseguir aprimorar o pulo

---
+++ mapa/assets/Background-1-sky.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Background-1-sky" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="Background/Background-1-sky.png" width="128" height="128"/>
+</tileset>
